cc841bec9919a208a7360b5efebe085c3207cf85
Dina P <dinchik@bk.ru>
2023-12-26 02:08:15 +0400

    feat: add solution for mine-sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
  * that don't contain a mine have a number in it that indicates the total number of mines
@@ -23,11 +21,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const mineSweeperGame = [],
+    coordinates = {};
+
+  for (let i = 0; i < matrix.length; i++) {
+    mineSweeperGame.push([]);
+    for (let j = 0; j < matrix[i].length; j++) {
+      coordinates[`${i}${j}`] = 0;
+      mineSweeperGame[i].push([]);
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j]) {
+        coordinates[`${i - 1}${j - 1}`]++;
+        coordinates[`${i - 1}${j}`]++;
+        coordinates[`${i - 1}${j + 1}`]++;
+
+        coordinates[`${i}${j - 1}`]++;
+        coordinates[`${i}${j + 1}`]++;
+
+        coordinates[`${i + 1}${j - 1}`]++;
+        coordinates[`${i + 1}${j}`]++;
+        coordinates[`${i + 1}${j + 1}`]++;
+      }
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      mineSweeperGame[i][j] = coordinates[`${i}${j}`];
+    }
+  }
+
+  return mineSweeperGame;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
