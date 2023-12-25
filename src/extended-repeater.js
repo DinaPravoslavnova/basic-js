a7897dd0be002f0e33cb70350de14278b5ef16b1
Dina P <dinchik@bk.ru>
2023-12-25 23:31:45 +0400

    feat: add solution for extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,24 +2,81 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let repeatingString = '';
+
+  if (str === null) str = 'null';
+
+  const fullOptions = {
+    repeatTimes: 1,
+    separator: '+',
+    addition: '',
+    additionRepeatTimes: 1,
+    additionSeparator: '|',
+  };
+
+  if (options.repeatTimes) {
+    fullOptions.repeatTimes = options.repeatTimes;
+  }
+
+  if (options.separator) {
+    fullOptions.separator = options.separator;
+  }
+
+  if (
+    options.addition ||
+    options.addition === false ||
+    options.addition === null
+  ) {
+    if (options.addition === false) {
+      fullOptions.addition = 'false';
+    } else if (options.addition === null) {
+      fullOptions.addition = 'null';
+    } else {
+      fullOptions.addition = options.addition;
+    }
+  }
+
+  if (options.additionRepeatTimes) {
+    fullOptions.additionRepeatTimes = options.additionRepeatTimes;
+  }
+  
+  if (options.additionSeparator) {
+    fullOptions.additionSeparator = options.additionSeparator;
+  }
+
+  for (let i = 0; i < fullOptions.repeatTimes; i++) {
+    repeatingString += str;
+
+    for (let j = 0; j < fullOptions.additionRepeatTimes; j++) {
+      repeatingString +=
+        fullOptions.additionRepeatTimes === 1 ||
+        fullOptions.additionRepeatTimes === j + 1
+          ? fullOptions.addition
+          : fullOptions.addition + fullOptions.additionSeparator;
+    }
+
+    if (fullOptions.repeatTimes !== i + 1) {
+      repeatingString += fullOptions.separator;
+    }
+  }
+
+  return repeatingString;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
