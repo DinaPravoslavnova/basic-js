6c1fc12275107379fe0dfc27144284e1e3e5240d
Dina P <dinchik@bk.ru>
2023-12-25 21:42:33 +0400

    feat: add solution for encode-line task

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given a string, return its encoding version.
  *
@@ -10,11 +8,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let resultEncodeLine = '',
+    countSimilarChar = 1;
+
+  if (str.length === 0) return str;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      countSimilarChar++;
+    } else {
+      if (countSimilarChar > 1) {
+        resultEncodeLine += countSimilarChar.toString() + str[i];
+        countSimilarChar = 1;
+      } else {
+        resultEncodeLine += str[i];
+        countSimilarChar = 1;
+      }
+    }
+  }
+
+  return resultEncodeLine;
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
